f1f7b7ece2f1baebd03344b75dc1ad821aa9ff7b
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2021-03-16 21:47:43 +0800

    finalized

---
+++ src/components/layouts/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const PageNotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>This page you find does not exist...</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
---
+++ src/components/users/Search.js
@@ -0,0 +1,54 @@
+import React, { useState, Fragment, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text === '') {
+      alertContext.setAlert('Please enter something', 'light');
+    } else {
+      githubContext.searchUsers(text);
+      setText('');
+    }
+  };
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='search user'
+          value={text}
+          onChange={handleChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className='btn-light btn-block'
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </Fragment>
+  );
+};
+
+export default Search;
